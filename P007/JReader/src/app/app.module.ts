a2826c9146f7569dc12a20bbc72e9cfa2cbf52be
Myllena <mssilva.egq@uesc.br>
2024-01-16 22:15:48 -0300

    criando aplicação e componentes

---
+++ P007/JReader/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ClassesComponent } from './classes/classes.component';
+import { PropriedadesComponent } from './propriedades/propriedades.component';
+import { ValorPropriedadeComponent } from './valor-propriedade/valor-propriedade.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarrinhoComponent,
+    ClassesComponent,
+    PropriedadesComponent,
+    ValorPropriedadeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
