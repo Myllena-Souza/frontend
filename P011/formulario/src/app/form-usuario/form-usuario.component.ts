16747b935470e06fb348285a2b215af3a0f816ac
Myllena <mssilva.egq@uesc.br>
2024-02-20 22:10:08 -0300

    att p011

--- P011/formulario/src/app/form-usuario/form-usuario.component.ts
+++ P011/formulario/src/app/form-usuario/form-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-usuario',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './form-usuario.component.css'
 })
 export class FormUsuarioComponent {
+  exemploForm: FormGroup;
+
+  constructor() { 
+    this.exemploForm = new FormGroup({
+      'login': new FormControl(null, Validators.required),
+      'senha': new FormControl(null),
+    });
+  }
+  onSubmit(){
+    console.log(this.exemploForm);
+    console.log(this.exemploForm.value);
+  }
 
 }
