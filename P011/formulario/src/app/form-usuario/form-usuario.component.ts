76371cb35584f4546186041f4389c9ff3444e5ac
Myllena <mssilva.egq@uesc.br>
2024-02-20 22:10:20 -0300

    att p001

--- P011/formulario/src/app/form-usuario/form-usuario.component.ts
+++ P011/formulario/src/app/form-usuario/form-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-usuario',
@@ -7,17 +7,60 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './form-usuario.component.css'
 })
 export class FormUsuarioComponent {
-  exemploForm: FormGroup;
+  exemploForm: FormGroup | undefined;
+  userForm: FormGroup<{ username: FormControl<string | null>; password: FormControl<string | null>; email: FormControl<string | null>; fullName: FormControl<string | null>; phone: FormControl<string | null>; address: FormControl<string | null>; birthDate: FormControl<string | null>; gender: FormControl<string | null>; profession: FormControl<string | null>; agreeTerms: FormControl<boolean | null>; }>;
 
-  constructor() { 
-    this.exemploForm = new FormGroup({
-      'login': new FormControl(null, Validators.required),
-      'senha': new FormControl(null),
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: ['', [Validators.required, Validators.maxLength(12), this.noWhitespaceValidator]],
+      password: ['', [Validators.required, Validators.minLength(4), this.passwordValidator]],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, this.fullNameValidator]],
+      phone: ['', [Validators.required, this.phoneValidator]],
+      address: ['', Validators.required],
+      birthDate: ['', [Validators.required, this.ageValidator]],
+      gender: ['', Validators.required],
+      profession: ['', Validators.required],
+      agreeTerms: [false, Validators.requiredTrue]
     });
   }
-  onSubmit(){
-    console.log(this.exemploForm);
-    console.log(this.exemploForm.value);
+
+  noWhitespaceValidator(control: AbstractControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { 'whitespace': true };
+  }
+
+  passwordValidator(control: AbstractControl) {
+    const hasUpperCase = /[A-Z]/.test(control.value);
+    const hasSymbol = /\W/.test(control.value);
+    const isValid = hasUpperCase && hasSymbol;
+    return isValid ? null : { 'password': true };
+  }
+
+  fullNameValidator(control: AbstractControl) {
+    const hasSpace = /\s/.test(control.value);
+    return hasSpace ? null : { 'fullName': true };
+  }
+
+  phoneValidator(control: AbstractControl) {
+    const isValidPhone = /\(\d{2}\) \d{4,5}-\d{4}/.test(control.value);
+    return isValidPhone ? null : { 'phone': true };
+  }
+
+  ageValidator(control: AbstractControl) {
+    const birthDate = new Date(control.value);
+    const ageDifMs = Date.now() - birthDate.getTime();
+    const ageDate = new Date(ageDifMs);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    return age >= 18 ? null : { 'age': true };
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const user = this.userForm.value;
+      console.log(user);
+    }
   }
 
 }
